ef0f1aa772c6e659f3824ff21a6a0e231768a30b
Victor J. Diaz <victorjadiaz@outlook.com>
2022-11-10 10:24:41 +0100

    test: Replace wildcard by individual imports
    Signed-off-by: Victor J. Diaz <victorjadiaz@outlook.com>

--- rosco-manifests/src/test/java/com/netflix/spinnaker/rosco/manifests/helm/HelmTemplateUtilsTest.java
+++ rosco-manifests/src/test/java/com/netflix/spinnaker/rosco/manifests/helm/HelmTemplateUtilsTest.java
@@ -18,7 +18,9 @@ package com.netflix.spinnaker.rosco.manifests.helm;
 
 import static com.netflix.spinnaker.rosco.manifests.ManifestTestUtils.makeSpinnakerHttpException;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doThrow;
